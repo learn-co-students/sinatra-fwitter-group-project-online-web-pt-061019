38addb7ed4cc5136182fbcfa1b07567f7279e2bb
alicia <afasciocco@gmail.com>
2019-09-29 09:35:44 -0400

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,10 @@
 class TweetsController < ApplicationController
-
-
+    get '/tweets' do 
+        if logged_in?
+            @user = current_user
+        erb :"/tweets/tweets"
+        else 
+            redirect "/login"
+        end
+    end
 end
