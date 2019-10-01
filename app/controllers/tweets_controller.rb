bdfbe10975d013c57995cd5a9778cea042e11264
Brittany Javalera <brittjavalera@gmail.com>
2019-09-30 21:39:25 -0700

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,41 @@
 class TweetsController < ApplicationController
+ get '/tweets' do
+   if Helpers.is_logged_in?(session)
+    redirect "/login"
+   else
+    @tweets = Tweet.all
+    erb :"tweets/tweets"
+ end
+end
 
+ get '/tweets/new' do
+    @tweets = Tweet.all
+    erb :"/tweets/new" 
+ end
 
+ post '/tweets' do
+    @tweet = Tweet.create(params[:tweet])
+    @tweet.save
+    redirect to "/tweets/#{@tweet.id}"
+ end
+
+ get '/tweet/:id' do
+    @tweet = Tweet.find(params[:id])
+   erb :"/tweets/show"
+ end
+
+get '/tweet/:id/edit' do
+    @tweet = Tweet.find(params[:id])
+    erb :"/tweets/edit"
+end
+
+patch '/tweets/:id' do
+    @tweet = Tweet.find(params[:id])
+    if params[:tweet][:content].empty?
+        redirect "/tweets/#{@tweet.id}/edit"
+    end
+    @tweet.update(params[:tweet])
+    @tweet.save
+    redirect "/tweets/#{@tweet.id}"
+end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,35 @@
+require 'pry'
 class UsersController < ApplicationController
 
+    get '/signup' do
+        if Helpers.is_logged_in?(session)
+          redirect '/tweets'
+        end
+        erb :'/users/create_user'
+    end
+  
+    post '/signup' do
+        #{"username"=>"skittles123", "email"=>"skittles@aol.com", "password"=>"rainbows"} 
+      params.each do |field, input|
+        if input.empty?
+          redirect '/signup'
+        end
+      end
+      @user = User.create(params)
+      session[:user_id] = @user.id
+      redirect '/tweets'
+    end
+
+    get '/login' do
+     if Helpers.is_logged_in?(session)
+        redirect to '/tweets'
+     end
+     erb :"/users/login"
+    end
+
+    post '/login' do
+     @user = User.find(params[:id])
+       erb :'/users/login'
+     end
 
 end
