a0b4d800643f65aeac385ce660b519015f97e79d
Brittany Javalera <brittjavalera@gmail.com>
2019-09-30 22:03:25 -0700

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,34 +2,39 @@ require 'pry'
 class UsersController < ApplicationController
 
     get '/signup' do
-        if Helpers.is_logged_in?(session_hash)
-          redirect '/tweets'
+        if Helpers.is_logged_in?(session)
+          redirect to '/tweets'
         end
-        erb :'/users/create_user'
-    end
+        erb :"/users/create_user"
+      end
   
     post '/signup' do
-        #{"username"=>"skittles123", "email"=>"skittles@aol.com", "password"=>"rainbows"} 
-      params.each do |field, input|
-        if input.empty?
-          redirect '/signup'
+        #{"username"=>"skittles123", "email"=>"skittles@aol.com", "password"=>"rainbows"}
+        params.each do |label, input|
+          if input.empty?
+            redirect to '/signup'
+          end
         end
+        user = User.create(params)
+        session[:user_id] = user.id
+        redirect to '/tweets'
       end
-      @user = User.create(params)
-      session[:user_id] = @user.id
-      redirect '/tweets'
-    end
 
     get '/login' do
-     if Helpers.is_logged_in?(session)
-        redirect to '/tweets'
-     end
-     erb :"/users/login"
-    end
+        if Helpers.is_logged_in?(session)
+            redirect to '/tweets'
+        end
+        erb :"/users/login"
+        end
 
     post '/login' do
-     @user = User.find(params[:id])
-       erb :'/users/login'
+        user = User.find_by(params[:username])
+        if user && user.authenticate(params[:password])
+          session[:user_id] = user.id
+          redirect to '/tweets'
+        else
+          redirect to '/login'
+        end
      end
 
 end
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,9 +1,9 @@
 class Helpers
-    def self.current_user(session_hash)
-      @user = User.find(session_hash[:user_id]) # Returns the object associated with the user id
+    def self.current_user(session)
+      @user = User.find(session[:user_id]) # Returns the object associated with the user id
     end
   
-    def self.is_logged_in?(session_hash)
-      !!session_hash[:user_id] # Turns into a boolean
+    def self.is_logged_in?(session)
+      !!session[:user_id] # Turns into a boolean
     end
   end
