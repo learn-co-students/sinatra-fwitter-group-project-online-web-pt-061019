717485f9131775e505dd489074cfea7e0c698347
Brittany Javalera <brittjavalera@gmail.com>
2019-10-01 21:38:54 -0700

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,19 +1,18 @@
 class TweetsController < ApplicationController
  get '/tweets' do
-    if Helpers.is_logged_in?(session)
+  if Helpers.is_logged_in?(session)
     @tweets = Tweet.all
     erb :"/tweets/tweets"
-    else
-        redirect to '/login'
-    end
-end
+  else
+    redirect to '/login'
+  end
+ end
 
  post '/tweets' do
     @tweet = Tweet.create(params[:tweet])
     @tweet.save
     redirect to "/tweets/#{@tweet.id}"
  end
-
 
  get '/tweets/new' do
     if Helpers.is_logged_in?(session)
@@ -36,6 +35,7 @@ get '/tweet/:id/edit' do
     if Helpers.is_logged_in?(session)
     @tweet = Tweet.find(params[:id])
     erb :"/tweets/edit_tweet"
+    end
 end
 
 patch '/tweets/:id' do
