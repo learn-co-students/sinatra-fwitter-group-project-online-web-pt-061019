1f9bfe0906207a97d4d14263ceb4dd995fb743c3
Sam Markham <sam.e.markham@gmail.com>
2019-10-05 16:50:29 -0700

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,55 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    if logged_in?
+      redirect "/tweets"
+    else
+      erb :'users/create_user'
+    end
+  end
+
+  post '/signup' do
+    #should log the user in and add the user_id
+    #to the sessions hash
+    #also add the link to the homepage
+    @user = User.new(params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect "/tweets"
+    else
+      redirect "/signup"
+    end
+  end
+
+  get '/login' do
+    if logged_in?
+      redirect "/tweets"
+    else
+      erb :'users/login'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/tweets'
+    else
+      redirect 'login'
+    end
+  end
+
+  get '/logout' do
+    session.destroy
+    redirect '/login'
+  end
+
+  get "/users/:slug" do
+    @user = User.find_by_slug(params[:slug])
+    @tweets = @user.tweets
+    erb :'users/show'
+  end
 
 end
