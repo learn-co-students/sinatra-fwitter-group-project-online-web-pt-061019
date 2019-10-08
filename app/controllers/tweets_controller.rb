c90a0c29d1ffa4ce33695c2a4a8cd6ba4b0fb6cf
maasa333 <konomaasa@gmail.com>
2019-10-08 06:52:17 -0700

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -13,6 +13,8 @@ class TweetsController < ApplicationController
         if logged_in?
             @user = current_user
             erb :'/tweets/new'
+        else
+            redirect '/login'
         end
     end 
 
@@ -24,5 +26,48 @@ class TweetsController < ApplicationController
             redirect '/tweets/new'
         end
         erb :'/tweets/tweets'
+    end
+
+    get '/tweets/:id' do 
+        if logged_in? 
+            @tweet = Tweet.find_by_id(params[:id])
+            if @tweet
+                erb :'/tweets/show_tweet'
+            else
+                redirect '/tweets'
+            end
+        else 
+            redirect '/login'
+        end
+    end
+
+    get '/tweets/:id/edit' do 
+        if logged_in? 
+            user = Tweet.find_by_id(params[:id]).user 
+            if user == current_user
+                @tweet = Tweet.find_by_id(params[:id])
+                erb :'/tweets/edit_tweet'
+            else
+                redirect '/tweets'
+            end
+        else
+            redirect '/login' 
+        end
+    end 
+
+    patch '/tweets/:id' do 
+        if logged_in?
+            @tweet = Tweet.find_by_id(params[:id])
+            binding.pry
+            if @tweet.user == current_user
+                if @tweet.update(content: params[:content])
+                    redirect "/tweets/#{@tweet.id}"
+                else
+                    redirect "/tweets/#{@tweet.id}/edit"
+                end
+            end
+        else
+            redirect '/login'
+        end
     end
 end
